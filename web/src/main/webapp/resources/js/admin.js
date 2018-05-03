f7a8b97c610168a42b47996e76137a7f58564103
Samuseva Anna <samuseva.a.s@mail.ru>
2018-05-03 20:14:42 +0300

    Choose Deposit change

--- web/src/main/webapp/resources/js/admin.js
+++ web/src/main/webapp/resources/js/admin.js
@@ -1,54 +1,5 @@
 $().ready(function(){
 
-    $("#registrationUser").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/registrationUser.html");
-    });
 
-    $("#addDeposit").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/addDeposit.html");
-    });
-
-    $("#addLoan").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/addLoan.html");
-    });
-    $("#changeKurs").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/changeKurs.html");
-    });
-    $("#openAccount").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/openAccount.html");
-    });
-    $("#openDeposit").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/openDeposit.html");
-    });
-    $("#openLoan").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/openLoan.html");
-    });
-    $("#plusAccount").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/plusAccount.html");
-    });
-    $("#searchUser").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/searchUser.html");
-    });
-    $("#showUsers").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/showUsers.html");
-    });
-    $("#viewDeposit").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/viewDeposit.html");
-    });
-    $("#viewLoan").click(function () {
-        $(".content").empty();
-        $(".content").load("../html/admin/viewLoan.html");
-    });
 
 });
